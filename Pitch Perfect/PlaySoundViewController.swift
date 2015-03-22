d80b3f109eec48a52379d6ff76588c05f959533a
mjaeger <huNt@h.home>
2015-03-22 12:46:34 +0100

    Added 2nd viewController and slow button.

---
+++ Pitch Perfect/PlaySoundViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PlaySoundViewController.swift
+//  Pitch Perfect
+//
+//  Created by jason on 22/03/15.
+//
+//
+
+import UIKit
+
+class PlaySoundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func slow_button_pressed(sender: UIButton) {
+        
+        
+    }
+}
--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -11,20 +11,46 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recording_label: UILabel!
+    @IBOutlet weak var stop_button: UIButton!
+
+    @IBOutlet weak var recording_button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.stop_button.hidden = true
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.recording_button.enabled = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func start_recording(sender: UIButton) {
-        
+    
+    func start_recording() {
         self.recording_label.hidden = !self.recording_label.hidden
+        
+        self.stop_button.hidden = false
+        self.recording_button.enabled = false
+    }
+    
+    func stop_recording() {
+        self.recording_label.hidden = true
+        self.recording_button.enabled = true
+        self.stop_button.hidden = true
     }
 
+    @IBAction func start_recording_pressed(sender: UIButton) {
+        
+        self.start_recording()
+    }
+
+    @IBAction func stop_button_pressed(sender: UIButton) {
+
+        self.stop_recording()
+    }
 }
 
