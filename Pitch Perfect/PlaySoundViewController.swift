cca330b9b67b256b98659d53b541f2f43d80fd9b
mjaeger <huNt@h.home>
2015-03-24 21:03:44 +0100

    Added fast button + functionality to play movie quote fast.

--- Pitch Perfect/PlaySoundViewController.swift
+++ Pitch Perfect/PlaySoundViewController.swift
@@ -13,6 +13,7 @@ class PlaySoundViewController: UIViewController {
 
     var audio_player: AVAudioPlayer? = nil
    
+  @IBOutlet weak var stop_button: UIButton!
     
     func read_movie_quote_file_path() -> String!{
       let file_path = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3")
@@ -22,10 +23,11 @@ class PlaySoundViewController: UIViewController {
     
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+      super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        self.read_movie_quote_file_path()
+      // Do any additional setup after loading the view.
+      self.read_movie_quote_file_path()
+      self.stop_button.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,16 +47,35 @@ class PlaySoundViewController: UIViewController {
     */
 
   @IBAction func slow_button_pressed(sender: UIButton) {
-      self.play_movie_quote()
+    self.play_movie_quote_slow()
+    self.stop_button.hidden = false
   }
     
   @IBAction func fast_button_pressed(sender: UIButton) {
+      self.play_movie_quote_fast()
+      self.stop_button.hidden = false
+  }
+  
+  @IBAction func stop_button_pressed(sender: UIButton) {
+    self.audio_player?.stop()
+  }
+  
+  
+  func play_movie_quote_fast() {
+    let file_path = self.read_movie_quote_file_path()
+    self.audio_player = self.init_audio_player_with_path(file_path)
+    self.play_sound_with_rate(2.0)
   }
     
-  func play_movie_quote() {
-    self.audio_player?.play()
-    self.audio_player = self.init_audio_player_with_path(self.read_movie_quote_file_path())
-    self.set_playback_rate(0.5)
+  func play_movie_quote_slow() {
+    let file_path = self.read_movie_quote_file_path()
+    self.audio_player = self.init_audio_player_with_path(file_path)
+    self.play_sound_with_rate(0.5)
+  }
+  
+  func play_sound_with_rate(rate:Float) {
+    self.set_playback_rate(rate)
+    self.audio_player?.stop()
     self.audio_player?.play()
   }
   
