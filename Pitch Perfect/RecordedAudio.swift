083fead09cfd4aefe4355c0489da52dff9eefc83
Michael Jaeger <Michael@addapptr.com>
2015-04-07 17:14:54 +0200

    Added commentary to RecordedAudio.swift. Supposingly the commentary is aimed at the wrong class.

--- Pitch Perfect/RecordedAudio.swift
+++ Pitch Perfect/RecordedAudio.swift
@@ -15,7 +15,25 @@ class RecordedAudio: NSObject {
     var title: String!
     var filePathURL: NSURL!
     
-    
+    /*I don't understand the review here.
+    This method should be the initializer, as I've found
+    in the swift book p22: 
+    “var name: String
+
+    init(name: String) {
+    self.name = name
+    }”
+
+    “Notice how self is used to distinguish the name property from the name argument to the initializer. The arguments to the initializer are passed like a function call when you create an instance of the class. Every property needs a value assigned—either in its declaration (as with numberOfSides) or in the initializer (as with name).”
+
+
+    Excerpt From: Apple Inc. “The Swift Programming Language.” iBooks. https://itun.es/de/jEUH0.l
+
+    I choose this initializer version because, from my point of view, 
+    an instance of this model should not exist without these properties.
+    Therefore, I want to "force" a user of this class to pass the relevant 
+    arguments directly.
+    */
     init (title:String!, filePathURL: NSURL!) {
         self.title = title
         self.filePathURL = filePathURL
