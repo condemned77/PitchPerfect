fa54e1350018b7cdb49498eef72261a656e48f17
Michael Jaeger <Michael@addapptr.com>
2015-04-08 17:15:48 +0200

    Finished commentary within class RecordSoundViewController.

--- Pitch Perfect/RecordSoundViewController.swift
+++ Pitch Perfect/RecordSoundViewController.swift
@@ -26,17 +26,17 @@ class RecordSoundViewController: UIViewController, AVAudioRecorderDelegate {
         // Do any additional setup after loading the view, typically from a nib.
         self.stop_button.hidden = true
     }
-
+    /*Each time the view controller appears, the recording button is enabled
+    and the label text below the recording button is adapted.*/
     override func viewWillAppear(animated: Bool) {
         self.recording_button.enabled   = true
         self.recording_label.text       = "Tap to Record"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    /*When the recording has finished, this method stores the recorded audio at a default file path
+    by using a model class called RecordedAudio.
+    Also the method automatically segues to another view controller.*/
     func audioRecorderDidFinishRecording(recorder: AVAudioRecorder!,
                                 successfully flag: Bool) {
         if flag {
@@ -54,6 +54,7 @@ class RecordSoundViewController: UIViewController, AVAudioRecorderDelegate {
         }
     }
 
+    /*The method passes the recorded audio, i.e. an RecordedAudio instance to the next view controller.*/
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "stopRecordingSegue" {
             let playSoundsVC            = segue.destinationViewController as PlaySoundViewController
@@ -61,11 +62,19 @@ class RecordSoundViewController: UIViewController, AVAudioRecorderDelegate {
         }
     }
 
-    func configure_UIButtons_when_start_recording_button_is_pressed(){
-        self.stop_button.hidden       = false
-        self.recording_button.enabled = false
-    }
 
+    /*This method assembles a default file name.
+    A default file name looks like this: 
+    08042015-081957.wav
+    Format:
+    ddMMyyyy-HHmmss
+    dd      = current day
+    MM      = current month
+    yyyy    = current year
+    HH      = current hour
+    mm      = current minute within an hour
+    ss      = current seconds within a minute
+    */
     func create_file_name_of_recording()-> String{
 
         let currentDateTime = NSDate()
@@ -88,7 +97,8 @@ class RecordSoundViewController: UIViewController, AVAudioRecorderDelegate {
         return NSURL.fileURLWithPathComponents(pathArray)!
     }
 
-
+    /*This method configures the AVAudioSessionCategory, which 
+    enables the app to report and play audio using the current AVAudioRecorder instance.*/
     func configure_AVAudioRecorder_session() {
         var session = AVAudioSession.sharedInstance()
         session.setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
@@ -116,23 +126,56 @@ class RecordSoundViewController: UIViewController, AVAudioRecorderDelegate {
         return audioRecorder
     }
 
+    /*This method handles the button properties that have to be set when the stop
+    recording button is pressed. 
+    When the button is pressed:
+    - the recording button is been re-enabled again, in order to record new audio.
+    - the stop button is set to hidden, since it's not used unless a recording is 
+    happening.*/
     func configure_UIButtons_when_stop_recording_button_is_pressed(){
         self.recording_button.enabled = true
         self.stop_button.hidden = true
     }
 
+    /*This method handles the button properties that have to be set when the start
+    recording button is pressed.
+    When the button is pressed:
+    - the stop button is made visible, since it's used to stop recording.
+    - the recording button is disabled, in order to avoid recording multiple times 
+    simultaniously.*/
+    func configure_UIButtons_when_start_recording_button_is_pressed(){
+        self.stop_button.hidden       = false
+        self.recording_button.enabled = false
+    }
+
+
+    /*This method purely handles the steps necessary to stop a recording.
+    That is:
+    - stopping the recording using the stop method on the current AVAudioRecorder instance. 
+    - disablding the current audio session.*/
     func stop_recording() {
         audioRecorder.stop()
         var audioSession = AVAudioSession.sharedInstance()
         audioSession.setActive(false, error: nil)
     }
 
+    /*This method execution a workflow when the start recording button is pressed.
+    That is:
+    - configuring the buttons on the current view controller
+    - adapting the label text below the recording button accordingly.
+    - start recording audio.*/
     @IBAction func start_recording_pressed(sender: UIButton) {
         self.configure_UIButtons_when_start_recording_button_is_pressed()
         self.recording_label.text = "recording"
         self.start_recording()
     }
 
+
+    /*This method executes a workflow when the stop recording button is pressed.
+    That is:
+    - adapting the label text below the recording button.
+    - configuring the buttons on the current view controller accordingly.
+    - stop recording audio.*/
     @IBAction func stop_button_pressed(sender: UIButton) {
         self.recording_label.text = "Tap to Record"
         self.configure_UIButtons_when_stop_recording_button_is_pressed()
